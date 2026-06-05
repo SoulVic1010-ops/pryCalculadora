d33f5341fc5fbfc3bc6e46d151d3a9f4235e078f
Yahir-07 <20251168@uthh.edu.mx>
2026-06-04 19:04:39 -0600

    hola

---
+++ pryCalculadora/ClsComparador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryCalculadora
+{
+    internal class ClsComparador : clsCalculadora
+    {
+
+		public string CompararNumero()
+		{
+			if (Numero1 > 5)
+				return "El número es mayor que 5";
+
+			if (Numero1 < 5)
+				return "El número es menor que 5";
+
+			return "El número es igual a 5";
+		}
+	}
+}
