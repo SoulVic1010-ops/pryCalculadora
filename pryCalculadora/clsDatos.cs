3dd62a3f0932d012f00fd34837cea88b3e4cc1a8
Gustavo Rodriguez <20251166@uthh.edu.mx>
2026-06-09 14:09:51 -0600

    subi datos

---
+++ pryCalculadora/clsDatos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryCalculadora
+{
+    internal class clsDatos
+    {
+        string nombre;
+        string apellido;
+    }
+}
